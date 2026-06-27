9649a246c7047b2190dfa9cca0638492b8b10b67
ishikajais27 <ishikajais09876@gmail.com>
2026-06-27 23:28:28 +0530

    The alarm that actually makes you move... or cry

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-import type { NextConfig } from 'next'
+// import type { NextConfig } from 'next'
 
-const config: NextConfig = {
+// const config: NextConfig = {
+//   webpack: (config) => {
+//     config.resolve.fallback = { fs: false, path: false }
+//     return config
+//   },
+// }
+
+// export default config
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false }
     return config
   },
 }
 
-export default config
+module.exports = nextConfig
